123fe40561cdf13a174100cea26a38356fda0728
rodrigocandidodev <rodrigo_candido.contato@outlook.com>
2024-05-27 09:50:45 -0300

    feat: interacao com o usuario criada

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("=== BEM-VINDO! ===");
+
+        System.out.println("Número da Conta: ");
+        int numero = scanner.nextInt();
+
+        System.out.println("Agência: ");
+        String agencia = scanner.next();
+
+        System.out.println("Saldo: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Nome: ");
+        String nome = scanner.next();
+
+        scanner.close();
+
+        System.out.println("Olá "+ nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque");
     }
 }
